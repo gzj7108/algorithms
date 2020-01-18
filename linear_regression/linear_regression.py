c21fd77030ecce8e7f9b2c4a394c8bb8c0ea9204
gzj7108 <798159825@qq.com>
2020-01-18 21:40:25 +0800

    Update linear_regression.py

--- linear_regression/linear_regression.py
+++ linear_regression/linear_regression.py
@@ -10,33 +10,39 @@ import numpy as np
 def linear_regression(w,x,y):
     """linear regression model
     input training data
-    w:weight and bias,ndarray
-    x:variable,ndarray
+    w:weight and bias
+    x:variable
     y:actual result
     output expected w
-    date:2020/2/17
+    date:2020/2/18
     """ 
 
     #set parameters
     learning_rate=0.001
     convergence_criterion=0.000001
-    iteration_times=10000
-    #g=2X^T(Xw-y)
-    gradient=2*np.transpose(x)*(x*w-y)
-    
+    iteration_times=40000
+    acceptale_error=0.0001
+
     #iteration
-    i=0
-    while i < iteration_times:
+    for i in range(iteration_times):
+       
+        #g=2X^T(Xw-y)
+        gradient=2*np.dot(x.transpose(),np.dot(x,w)-y)
+        #gradient iteration
         w=gradient_descent(w,gradient,learning_rate)
-        print(np.transpose(y-x*w)*(y-x*w))
-        i+=1
+        #gradient?0
+        if abs(np.dot(gradient.transpose(),gradient)) < acceptale_error:
+            print(" gradient convergence!")
+            break
 
     return w
 
+#test
 w=[0,0]
 w=np.array(w)
 x=[[1,1],[2,1]]
 x=np.array(x)
+#print(x.shape)
 y=[1,2]
-y=np.array(y)
+y=np.array(y).transpose()
 print(linear_regression(w,x,y))
