fe0868db1ac1a10026cdba919960543aeadc4946
gzj7108 <798159825@qq.com>
2020-01-18 21:40:44 +0800

    Update performance.py

--- linear_regression/performance.py
+++ linear_regression/performance.py
@@ -7,24 +7,27 @@ import numpy as np
 
 def average_square(a_r,t_r):
     """average square measure
-    input actual result,trained result,ndarray
-    output average square,float 
+    input actual result,trained result,column vector
+    output average square
     date:2020/1/17
     """
-    sum=0
+    
+    
     if len(a_r) == len(t_r):
         length=len(a_r)
+        sum=np.dot((a_r-t_r).transpose(),(a_r-t_r))/length
+        print((a_r-t_r).transpose().shape)
     else:
         print("average_square parameters don't match!")
         return
 
-    for i in range(0,length) :
-        sum+=(a_r[i]-t_r[i])**2
-    sum/=length
-
     return sum
 
 #test
-a=np.array([1,2,4])
-b=np.array([1,2,3])
+a=np.transpose([1,2,4])
+print(a,a.shape)
+b=np.transpose([1,2,3])
+
+
 print(average_square(a,b))
+print(abs(-1))
