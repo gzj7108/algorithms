45c131c9879cba8dfe46b3444c668fb1ca6d9e76
gzj7108 <798159825@qq.com>
2020-02-18 22:04:19 +0800

    Update performance.py

--- linear_regression/performance.py
+++ linear_regression/performance.py
@@ -5,29 +5,40 @@ performance measure
 
 import numpy as np
 
-def average_square(a_r,t_r):
-    """average square measure
+def MSE(a_r,t_r):
+    """mean squared error
     input actual result,trained result,column vector
     output average square
     date:2020/1/18
     """
-    
-    
-    if len(a_r) == len(t_r):
-        length=len(a_r)
-        sum=np.dot((a_r-t_r).transpose(),(a_r-t_r))/length
-        print((a_r-t_r).transpose().shape)
-    else:
-        print("average_square parameters don't match!")
-        return
+    length=len(a_r)
+    sum=np.dot((a_r-t_r).transpose(),(a_r-t_r))/length
 
     return sum
+
+def RMSE(a_r,t_r):
+
+    return MSE(a_r,t_r)**0.5
+
+def AMSE(a_r,t_r):
+    tmp=np.abs(a_r-t_r)
+    length=len(a_r)
+    sum=np.dot(tmp.transpose(),tmp)/length
+
+    return sum
+
+
+def R2(a_r,t_r):
+    m_r=np.mean(t_r)
+    length=len(a_r)
+    tse=np.dot((a_r-m_r).transpose(),(a_r-m_r))/length
+    cost=1-MSE(a_r,t_r)/tse
+    return cost
+
+
 
 #test
 a=np.transpose([1,2,4])
 print(a,a.shape)
 b=np.transpose([1,2,3])
-
-
-print(average_square(a,b))
 print(abs(-1))
